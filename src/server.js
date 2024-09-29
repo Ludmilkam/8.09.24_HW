b2a2b87b6778f7e253c1a920807b41a76df85a7e
Liudmyla Makhynia <milam20090929@gmail.com>
2024-09-29 14:54:22 +0300

    added Route parameters with styles

--- src/server.js
+++ src/server.js
@@ -6,6 +6,23 @@ const app = express()
 const HOST = '127.0.0.1' 
 const PORT = 8000
 
+const posts = [
+    {
+    id:"1",
+    name: 'post1',
+    description:" просто пост",
+    time_publicated:"сегодня",
+    author: 'Author1 '
+    },
+    {
+    id:"2",
+    name: 'post2',
+    description:" it`s my birthday",
+    time_publicated:"29.09",
+    author: 'Author2'
+    }
+]
+
 app.set("view engine", "ejs")
 app.set("views", path.resolve(__dirname, "./templates"))
 
@@ -25,6 +42,16 @@ app.get("/posts",(req,res) => {
         posts: [ {name: 'post1', author: 'Author1 '}, {name: 'post2', author: 'Author2'} ]
     }
     res.render("main", context)
+})
+app.get("/posts/:id",(req,res) => {
+    const id = req.params.id
+    console.log(id)
+
+    const context = {
+        post:posts[id-1],
+    }
+
+    res.render("post", context)
 })
 app.listen(PORT,HOST,()=>{
     console.log("Server is running on http://127.0.0.1:8000")
