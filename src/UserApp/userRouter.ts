3b868409dfc59fe34d3269513e8bcc09ba8be485
Liudmyla Makhynia <milam20090929@gmail.com>
2024-12-18 18:41:54 +0200

    fix

--- src/UserApp/userRouter.ts
+++ src/UserApp/userRouter.ts
@@ -4,9 +4,9 @@ import userController from "./userController"
 const router:Router = Router()
 
 router.get("/login", userController.loginUser)
-router.post("/login", userController.loginUser)
+router.post("/login", userController.authUser)
 router.get("/registration", userController.registerUser)
-router.post("/registration", userController.registerUser)
+router.post("/registration", userController.authRegisterUser)
 
 
 export default router
