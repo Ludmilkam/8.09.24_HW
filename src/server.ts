edd389c18e39fbd2b4055fccfb2236becfa1e88d
Liudmyla Makhynia <ludmilam20090929@gmail.com>
2025-02-17 21:01:23 +0200

    add api with cors

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,14 @@
-// const express = require('express')
-
-// const path = require('path')
-
-
 import express, {Express, Request, Response} from 'express'
 import path from 'path'
 import postRouter from "./PostApp/postRouter"
+import postRouterApi from "./PostApp/api/postRouterApi"
 import commentRouter from "./CommentApp/commentRouter"
 import userRouter from './UserApp/userRouter'
 // import getCurrentDate from './static/script.js'
 const app : Express = express()
 // const date = require('./static/script.js')
-// const postRouter = require("./routers/postRouter")
 import cookieParser from "cookie-parser"
+import cors from "cors"
 
 
 const HOST = '127.0.0.1' 
@@ -27,7 +23,12 @@ app.use("/static/", express.static(path.resolve(__dirname, "./static")))
 app.use(express.json())
 
 app.use(cookieParser())
+app.use(cors({
+    origin : ["http://localhost:3000"]
+}))
 
+
+app.use("/api/post/", postRouterApi)
 app.use("/post/", postRouter)
 app.use("/comment/", commentRouter)
 app.use("/", userRouter)
